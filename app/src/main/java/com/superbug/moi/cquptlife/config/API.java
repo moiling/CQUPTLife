549fb369a28f87740fce071e4f17e2a855d9def7
moiling <super8moi@gmail.com>
2015-11-06 22:53:47 +0800

    虽然什么都没做，但我就是要提交

--- app/src/main/java/com/superbug/moi/cquptlife/config/API.java
+++ app/src/main/java/com/superbug/moi/cquptlife/config/API.java
@@ -1,6 +1,5 @@
 package com.superbug.moi.cquptlife.config;
 
-
 public class API {
 
 
---
+++ app/src/main/java/com/superbug/moi/cquptlife/widget/SearchWidgetProvider.java
@@ -0,0 +1,36 @@
+package com.superbug.moi.cquptlife.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by moi on 11/1/2015.
+ */
+public class SearchWidgetProvider extends AppWidgetProvider {
+    // 没接收一次广播消息就调用一次，使用频繁
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    // 每次更新都调用一次该方法，使用频繁
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    // 没删除一个就调用一次
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    // 当该Widget第一次添加到桌面是调用该方法，可添加多次但只第一次调用
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    // 当最后一个该Widget删除是调用该方法，注意是最后一个
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+}
